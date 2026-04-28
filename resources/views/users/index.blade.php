71b5095d3f22df0ab0478d37f7ed1893d5cd370f
Chrizeelll <diannebosque17@gmail.com>
2026-04-28 21:37:33 +0800

    last???

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -38,7 +38,7 @@
                                 @foreach($users as $user)
                                     <tr>
                                         <td>{{ $user->johnelladonodn }}</td>
-                                        <td>{{ $user->email }}</td>
+                                        <td>{{ $user->diannebosque }}</td>
                                     </tr>
                                 @endforeach
                                 </tbody>
